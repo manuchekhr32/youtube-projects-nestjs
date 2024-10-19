1372c840faa8594e534011e9b3b910dcbdcd34e2
Manuchehr <manuchekhr.3232@gmail.com>
2024-10-19 16:44:26 +0500

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { EnvVars } from './shared/enum/env';
-import { CacheModule } from '@nestjs/cache-manager';
-import type { RedisClientOptions } from 'redis';
-import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -14,33 +11,17 @@ import { redisStore } from 'cache-manager-redis-store';
         [EnvVars.PORT]: Joi.number(),
         [EnvVars.SWAGGER]: Joi.bool(),
         [EnvVars.CORS]: Joi.string(),
-        // Redis
-        [EnvVars.REDIS_PASSWORD]: Joi.string(),
-        [EnvVars.REDIS_HOST]: Joi.string(),
-        [EnvVars.REDIS_PORT]: Joi.number(),
-        [EnvVars.REDIS_DB]: Joi.number(),
+        // Postgres
+        [EnvVars.DB_USER]: Joi.string(),
+        [EnvVars.DB_PASSWORD]: Joi.string(),
+        [EnvVars.DB_NAME]: Joi.string(),
+        [EnvVars.DB_HOST]: Joi.string(),
+        [EnvVars.DB_PORT]: Joi.number(),
       }),
       validationOptions: {
         allowUnknown: true,
         abortEarly: true,
       },
-    }),
-    CacheModule.registerAsync<RedisClientOptions>({
-      isGlobal: true,
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      // eslint-disable-next-line
-      // @ts-ignore
-      useFactory: async (configService: ConfigService) => ({
-        store: await redisStore({
-          socket: {
-            host: configService.getOrThrow(EnvVars.REDIS_HOST),
-            port: +configService.getOrThrow(EnvVars.REDIS_PORT),
-          },
-          password: configService.getOrThrow(EnvVars.REDIS_PASSWORD),
-          database: parseInt(configService.getOrThrow(EnvVars.REDIS_DB)),
-        }),
-      }),
     }),
   ],
 })
